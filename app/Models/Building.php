a9c8df3ee4c0aa0d171940d899214ead4174e743
amanda-araa20 <mandarazkya@gmail.com>
2024-12-19 17:15:58 +0700

    Update Building.php

--- app/Models/Building.php
+++ app/Models/Building.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Building extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'buildings';
+
+    protected $fillable = [
+        'name_building',
+        'floor',
+        'mapping',
+        'foto'
+    ];
 }
