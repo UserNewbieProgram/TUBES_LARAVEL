4eb9908ad8981790c15d5f1b1a09d249fbbec284
amanda-araa20 <mandarazkya@gmail.com>
2024-12-19 17:18:08 +0700

    Create 2024_12_17_083937_create_buildings_table.php

---
+++ app/Http/Controllers/2024_12_17_083937_create_buildings_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_building');
+            $table->integer('floor');
+            $table->string('mapping');
+            $table->string('foto')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buildings');
+    }
+};
