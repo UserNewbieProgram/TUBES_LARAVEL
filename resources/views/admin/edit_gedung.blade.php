c68a3638e38aaf06a6dad153308868add66e4562
amanda-araa20 <mandarazkya@gmail.com>
2024-12-22 20:32:08 +0700

    Update edit_gedung.blade.php

--- resources/views/admin/edit_gedung.blade.php
+++ resources/views/admin/edit_gedung.blade.php
@@ -64,6 +64,26 @@
             </ul>
         </li><!-- End Edit Data Nav -->
 
+        <li class="nav-item">
+            <a class="nav-link collapsed" data-bs-target="#hapuss-nav" data-bs-toggle="collapse" href="#">
+                <i class="bi bi-journal-text"></i><span>Hapus Data</span><i class="bi bi-chevron-down ms-auto"></i>
+            </a>
+            <ul id="hapuss-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
+                <li>
+                    
+                        <a href="{{ route('admin.hapus_gedung', ['id' => $building->id]) }}">
+                            <i class="bi bi-circle"></i><span>Data Gedung</span>
+                        </a>
+                    
+                </li>
+                <li>
+                    <a href="#">
+                        <i class="bi bi-circle"></i><span>Data Ruang</span>
+                    </a>
+                </li>
+            </ul>
+        </li><!-- End Delete Data Nav -->
+
         <li class="nav-item"><a class="nav-link collapsed" href="#"><i class="bi bi-layout-text-window-reverse"></i><span>Riwayat</span></a></li>
     </ul>
 </aside><!-- End Sidebar -->
