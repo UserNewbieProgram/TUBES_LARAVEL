5ea19f4bcfff6f529c0ae7c4d3fedb64709e909e
amanda-araa20 <mandarazkya@gmail.com>
2024-12-22 20:34:54 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -45,5 +45,6 @@ Route::get('/admin/form_gedung', [BuildingController::class, 'create'])->name('a
 Route::post('/building/store', [BuildingController::class, 'store'])->name('admin.store_gedung');
 Route::get('/admin/{id}/edit_gedung', [BuildingController::class, 'edit'])->name('admin.edit_gedung');
 Route::put('/admin/{id}/edit_gedung', [BuildingController::class, 'update'])->name('admin.update_gedung');
-// Route::delete('/admin/{id}', [BuildingController::class, 'destroy']);
+Route::delete('/admin/{id}/hapus_gedung', [BuildingController::class, 'destroy'])->name('admin.destroy_gedung');
+Route::get('/admin/{id}/hapus_gedung', [BuildingController::class, 'hapusGedung'])->name('admin.hapus_gedung');
 
