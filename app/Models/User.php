297126ed214ba747a5502f95b2366e07e66972a3
Aldyandra Tibriharsa R <155159261+Aldyandra01@users.noreply.github.com>
2024-12-28 14:32:19 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,6 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -23,31 +18,20 @@ class User extends Authenticatable
         'role',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
+ 
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+ 
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-    /**
-     * Relasi ke model lain.
-     * Misalnya: User memiliki banyak pengajuan.
-     */
+  
     public function pengajuans()
     {
         return $this->hasMany(Pengajuan::class);
-    }
+    }
 }
