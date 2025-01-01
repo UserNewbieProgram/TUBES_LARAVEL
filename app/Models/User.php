b53278844ad1d1ca1ca7c5fefe118cd0599a9e34
Aldyandra Tibriharsa R <155159261+Aldyandra01@users.noreply.github.com>
2025-01-01 12:14:26 +0700

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,11 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
     protected $fillable = [
         'name',
         'email',
@@ -18,20 +23,83 @@ class User extends Authenticatable
         'role',
     ];
 
- 
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
- 
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
 
-  
+    /**
+     * Relasi ke model lain.
+     * Misalnya: User memiliki banyak pengajuan.
+     */
     public function pengajuans()
     {
         return $this->hasMany(Pengajuan::class);
-    }
+    }
+}<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Relasi ke model lain.
+     * Misalnya: User memiliki banyak pengajuan.
+     */
+    public function pengajuans()
+    {
+        return $this->hasMany(Pengajuan::class);
+    }
 }
